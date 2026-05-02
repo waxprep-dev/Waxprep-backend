ea4a3a9e3d548eeed66a7752ba1ab11e4ba183c0
waxprep-dev <waxprepofficial@gmail.com>
2026-05-02 20:33:48 +0100

    Update handler.py

--- telegram/handler.py
+++ telegram/handler.py
@@ -358,7 +358,7 @@ async def _think_and_respond_telegram(chat_id: int, student: dict, conversation:
         await send_telegram_message(chat_id, response_text, reply_markup=keyboard)
 
         bg_task(save_message(conversation['id'], student['id'], 'telegram', 'assistant', response_text))
-        asyncio.ensure_future(increment_questions_today(student['id']))
+        bg_task(increment_questions_today(student['id']))
 
         if question_data:
             from features.recent_questions import add_recent_question
